a9bad16f57ea95fb97968d909bfb970b4136eabb
RNBPRA <bhavaniprasadramisetti999@gmail.com>
2024-06-24 08:44:49 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
+import Admin from './Components/Admin';
+import Query from './Components/Query';
+import Status from './Components/Status';
+import bannerImage from './Assets/AP.png'; // Make sure to replace 'Aadhra Pradesh.png' with your actual image file name
+import Navbar from './Components/NavBar';
 
 function App() {
+  console.log(bannerImage); // Log the image path to check if it's resolved correctly
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header" style={{ backgroundImage: `url(${bannerImage})` }}>
+          {/* Add your header content here */}
+        </header>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/query" element={<Query />} />
+            <Route path="/status" element={<Status />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,25 @@
+// src/Navbar.js
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/"></Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav ms-auto">
+          <Link className="nav-item nav-link" to="/">Home</Link>
+          <Link className="nav-item nav-link" to="/status">Status</Link>
+          <Link className="nav-item nav-link" to="/query">Query</Link>
+          <Link className="nav-item nav-link" to="/admin">Admin</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
