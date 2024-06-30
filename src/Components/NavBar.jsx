7a2e84d148b4d7e5cee085b55a9a9879cfc6f99e
VenkateshDulla <vdulla@haryak.com>
2024-06-30 19:14:42 +0530

    a small change in Navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'; // Use NavLink instead of Link
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">Home</Link>
+      <NavLink className="navbar-brand" to="/">Home</NavLink>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -14,18 +14,8 @@ function Navbar() {
           <NavLink className="nav-item nav-link" exact to="/">Home</NavLink>
           <NavLink className="nav-item nav-link" to="/status">Status</NavLink>
           <NavLink className="nav-item nav-link" to="/query">Query</NavLink>
-          <NavLink className="nav-item nav-link" to="/dashboard">Dashboard</NavLink> {/* Move Dashboard link here */}
-          <div className="nav-item dropdown">
-            <NavLink className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Admin
-            </NavLink>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <NavLink className="dropdown-item" to="/mro">MRO</NavLink>
-              <NavLink className="dropdown-item" to="/ias">IAS</NavLink>
-              <NavLink className="dropdown-item" to="/cm">CM</NavLink>
-            </div>
-          </div>
-          <NavLink className="nav-item nav-link" to="/admin">Login</NavLink>
+          <NavLink className="nav-item nav-link" to="/admin">Login</NavLink>  
+          <NavLink className="nav-item nav-link" to="/dashboard">Dashboard</NavLink>  
         </div>
       </div>
     </nav>
